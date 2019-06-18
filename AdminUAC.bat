31473d9aebe6eceb224f89ae7d08c2a5100f2a30
crazed014 <50474893+crazed014@users.noreply.github.com>
2019-06-18 15:05:54 -0600

    Update AdminUAC.bat

--- AdminUAC.bat
+++ AdminUAC.bat
@@ -1,2 +1,3 @@
-Powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System\" -Name "ConsentPromptBehaviorAdmin" -Value 3
-"
+if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
+
+Powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System\" -Name "ConsentPromptBehaviorAdmin" -Value 3"
